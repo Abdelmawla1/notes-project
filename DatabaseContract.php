5cb0d4625bc8fef6ce8b643256f72d8eb60d31eb
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-04-04 21:01:25 +0200

    Adding Database Contract

---
+++ DatabaseContract.php
@@ -0,0 +1,24 @@
+<?php
+
+interface DatabaseContract
+{
+    public function insert(array $data);
+
+    public function update(array $data);
+
+    public function select(string $column = "*");
+
+    public function delete();
+
+    public function execute();
+
+    public function getRow();
+
+    public function getAllRows();
+
+    public function where(string $column, string $operator, $value);
+
+    public function andWhere(string $column, string $operator, $value);
+
+    public function orWhere(string $column, string $operator, $value);
+}
