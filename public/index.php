6d2cd9e4fefa1376dd1313d358c187f2f9c3ba94
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-04-08 20:26:25 +0200

    Adding const BASE_PATH to index.php and transferring Database.php, Response.php, Validator.php classes , DatabaseContract.php interface and functions.php file to Core directory

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
+
+// This const (BASE_PATH) will point to a path and absolute path to the root of the project
+// some programmers call that const SPACE_PATH
+// You can concatenate it to the require below and in the others files, but we will use different approach by using function.
+const BASE_PATH = __DIR__ . '/../';
+
+// We can't call base_path() here because it's for functions.php and that helper function doesn't exist yet.
+require BASE_PATH . "functions.php";
+
+
 require "router.php";
 //echo "Notes Project";
