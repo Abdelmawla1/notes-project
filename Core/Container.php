a609d4cb5de1552b9f4300c2d739f99da751b781
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-04-11 20:09:24 +0200

    Making Container.php class

---
+++ Core/Container.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core;
+
+class Container
+{
+    protected array $bindings = [];
+
+    public function bind($key, $factoryFunction): void
+    {
+        $this->bindings[$key] = $factoryFunction;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function resolve($key): void
+    {
+        if(! array_key_exists($key,$this->bindings)){
+            throw new \Exception("No matching bindings found for {$key}");
+        }
+        $factoryFunction = $this->bindings[$key];
+
+        call_user_func($factoryFunction);
+    }
+
+}
