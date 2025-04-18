72fefc70214111b226b705fc5fa29f641a807a0d
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-04-18 20:58:00 +0200

    Completing create.php and adding store.php which are related to registration

---
+++ controllers/registration/store.php
@@ -0,0 +1,40 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$db = App::resolve(Database::class);
+
+$errors = [];
+
+if (! Validator::email($_POST['email'])) {
+    $errors['email'] = "Please provide a valid email address.";
+}
+
+if (! Validator::string($_POST['password'], 6, 255)) {
+    $errors['password'] = "Please provide a password of at least seven characters.";
+}
+
+if (! empty($errors)) {
+    view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
+}
+$user = $db->table('users')->select()->where('email', '=', $_POST['email'])->execute()->getRow();
+if ($user) {
+    header('location: /');
+    exit();
+} else {
+    $db->table('users')->$db->table('users')->insert([
+        'email' => $_POST['email'],
+        'password' => password_hash($_POST['password'], PASSWORD_BCRYPT)
+    ])->execute();
+
+    $_SESSION['user'] = [
+        'email' => $_POST['email']
+    ];
+
+    header('location: /');
+    exit();
+}
