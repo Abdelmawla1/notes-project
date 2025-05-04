d32867162484083fdd0e26999e968b5527744464
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-05-05 00:49:19 +0300

    Adding methods to session

--- Core/Session.php
+++ Core/Session.php
@@ -4,5 +4,28 @@ namespace Core;
 
 class Session
 {
+    public static function put($key, $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
 
+    public static function get($key, $default = null)
+    {
+        return $_SESSION[$key] ?? $default;
+    }
+
+    public static function has($key): bool
+    {
+        return (bool) static::get($key);
+    }
+
+    public static function flash($key, $value): void
+    {
+        $_SESSION['_flash'][$key] = $value;
+    }
+
+    public static function unFlash(): void
+    {
+        unset($_SESSION['_flash']);
+    }
 }
