aebae868c83552cc8bbbd7c219ab24c8eadc758a
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-05-06 01:56:06 +0300

    Adding flush and destroy methods

--- Core/Session.php
+++ Core/Session.php
@@ -31,4 +31,21 @@ class Session
     {
         unset($_SESSION['_flash']);
     }
+
+    public static function flush(): void
+    {
+        $_SESSION = [];
+    }
+
+    public static function destroy(): void
+    {
+        static::flush();
+
+        session_destroy();
+
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID','',time() - 3600,$params['path'],$params['domain'],$params['secure'],$params['httponly']);
+
+
+    }
 }
