f094c70900db130cb90892fdc475baeb62b0fbe6
Abdelmawla1 <mahmoud.sayed.abdelmawla@gmail.com>
2025-05-08 04:01:41 +0300

    Updating LoginForm

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -2,23 +2,37 @@
 
 namespace Http\Forms;
 
+use Core\ValidationException;
 use Core\Validator;
 
 class LoginForm
 {
     protected array $errors = [];
 
-    function validate($email, $password): bool
+    public function __construct(public array $attributes)
     {
-        if (!Validator::email($email)) {
+        if (!Validator::email($this->attributes['email'])) {
             $this->errors ['email'] = 'Please provide a valid email address.';
         }
 
-        if (!Validator::String($password)) {
+        if (!Validator::String($this->attributes['password'])) {
             $this->errors ['password'] = 'Please provide a valid password.';
         }
+    }
 
-       return empty($this->errors);
+    public static function validate($attributes)
+    {
+        $instance = new static($attributes);
+
+        if ($instance->hasErrors()) {
+            ValidationException::throw($instance->getErrors(), $instance->attributes);
+        }
+        return $instance;
+    }
+
+    public function hasErrors(): int
+    {
+        return count($this->errors);
     }
 
     public function getErrors(): array
